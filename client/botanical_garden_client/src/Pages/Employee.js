717424262938fd9d4152b573ef707b41633b52c9
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-04-29 15:57:43 +0200

    Pages init

---
+++ client/botanical_garden_client/src/Pages/Employee.js
@@ -0,0 +1,9 @@
+
+export default function Employee(){
+
+    return(
+        <div>
+            <h1>Employee Page</h1>
+        </div>
+    )
+}
---
+++ client/botanical_garden_client/src/Pages/Guest.js
@@ -0,0 +1,9 @@
+
+export default function Guest(){
+
+    return(
+        <div>
+            <h1>Guest Page</h1>
+        </div>
+    )
+}
