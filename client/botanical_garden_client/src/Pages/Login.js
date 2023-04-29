b14bf970de120b5e575b92e88e48c417f60d11d3
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-04-29 18:30:46 +0200

    make loginForm visible when hovering over text div

--- client/botanical_garden_client/src/Pages/Login.js
+++ client/botanical_garden_client/src/Pages/Login.js
@@ -1,37 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+import { useState } from 'react';
+
 export default function Login(){
 
-    return(
+    const [showLoginForm, setShowLoginForm] = useState(false);
+
+    const handleMouseEnter = () => {
+        setShowLoginForm(true);
+    }
+
+    const handleMouseLeave = () => {
+        setShowLoginForm(false);
+    }
+
+    return (
         <div id="loginPage">
 
-            <div id="text">Grădina Botanică Alexandru Borza</div>
+            <div id="container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <div id="text">Grădina Botanică Alexandru Borza</div>
 
-            {/*
-            <div id="loginForm">
-                 <div id="credentials">
-                     <div className="form-floating mb-3">
-                         <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
-                             <label htmlFor="floatingInput">Nume utilizator</label>
-                     </div>
+                <div id="loginForm" style={{ display: showLoginForm ? 'block' : 'none' }}>
+                    <div id="credentials">
+                        <div className="form-floating mb-3">
+                            <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                            <label htmlFor="floatingInput">Nume utilizator</label>
+                        </div>
 
-                     <div className="form-floating mb-3">
-                         <input type="password" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
-                         <label htmlFor="floatingInput">Parolă</label>
-                     </div>
-                 </div>
+                        <div className="form-floating mb-3">
+                            <input type="password" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                            <label htmlFor="floatingInput">Parolă</label>
+                        </div>
+                    </div>
 
-                 <div id="buttonsLogin">
-                     <button type="button" className="btn btn-success">Autentificare</button>
-                     <button type="button" className="btn btn-success">Vizualizare ca vizitator</button>
+                    <div id="buttonsLogin">
+                        <button type="button" className="btn btn-success">Autentificare</button>
+                        <button type="button" className="btn btn-success">Vizualizare ca vizitator</button>
 
-                 </div>
+                    </div>
 
-             </div>  */}
-
-
-
+                </div>
+            </div>
         </div>
     )
 }
