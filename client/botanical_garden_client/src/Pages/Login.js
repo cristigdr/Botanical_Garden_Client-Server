a27556c0ffdb6e186282e96cb7d65c9b0a76daeb
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-04-29 18:20:43 +0200

    Login Page

--- client/botanical_garden_client/src/Pages/Login.js
+++ client/botanical_garden_client/src/Pages/Login.js
@@ -1,8 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 export default function Login(){
 
     return(
-        <div>
-            <h1>Login Page</h1>
+        <div id="loginPage">
+
+            <div id="text">Grădina Botanică Alexandru Borza</div>
+
+            {/*
+            <div id="loginForm">
+                 <div id="credentials">
+                     <div className="form-floating mb-3">
+                         <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                             <label htmlFor="floatingInput">Nume utilizator</label>
+                     </div>
+
+                     <div className="form-floating mb-3">
+                         <input type="password" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                         <label htmlFor="floatingInput">Parolă</label>
+                     </div>
+                 </div>
+
+                 <div id="buttonsLogin">
+                     <button type="button" className="btn btn-success">Autentificare</button>
+                     <button type="button" className="btn btn-success">Vizualizare ca vizitator</button>
+
+                 </div>
+
+             </div>  */}
+
+
+
         </div>
     )
 }
