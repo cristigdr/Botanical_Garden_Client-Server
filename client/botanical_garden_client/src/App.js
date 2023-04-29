b7a9d9f03d6c10b64e448fc0dc3702c808e62b9b
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-04-29 16:06:08 +0200

    Routes

--- client/botanical_garden_client/src/App.js
+++ client/botanical_garden_client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import Guest from "./Pages/Guest";
+import Employee from "./Pages/Employee";
+import Admin from "./Pages/Admin";
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/guest" element={<Guest />} />
+          <Route path="/employee" element={<Employee />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Router>  );
 }
 
 export default App;
