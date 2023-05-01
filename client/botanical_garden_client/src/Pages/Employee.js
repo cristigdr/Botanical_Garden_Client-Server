004669e0cdde6a25cd137d041581c873a1100ab4
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-05-01 13:37:25 +0200

    employee page search form and table

--- client/botanical_garden_client/src/Pages/Employee.js
+++ client/botanical_garden_client/src/Pages/Employee.js
@@ -1,9 +1,87 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import {useState} from "react";
 
 export default function Employee(){
+    const [buttonText, setButtonText] = useState('Criteriu');
 
+    function handleItemClick(event) {
+        setButtonText(event.target.innerText);
+    }
+
+    const [showSearchForm, setshowSearchForm] = useState(false);
+
+    const handleMouseEnter = () => {
+        setshowSearchForm(true);
+    }
+
+    const handleMouseLeave = () => {
+        setshowSearchForm(false);
+    }
     return(
-        <div>
-            <h1>Employee Page</h1>
+        <div id ="employeePage">
+            <div className="text" onMouseEnter={handleMouseEnter}>Bun venit!</div>
+
+            <div id="searchForm" style={{ display: showSearchForm ? 'block' : 'none' }} onMouseLeave={handleMouseLeave}>
+
+                <div className="btn-group dropdown">
+                    <button type="button" className="btn btn-success dropdown-toggle" data-bs-toggle="dropdown" data-bs-trigger="hover" aria-expanded="false">
+                        {buttonText}
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="#" onClick={handleItemClick}>Denumire</a></li>
+                        <li><a className="dropdown-item" href="#" onClick={handleItemClick}>Tip</a></li>
+                        <li><a className="dropdown-item" href="#" onClick={handleItemClick}>Specie</a></li>
+                        <li><a className="dropdown-item" href="#" onClick={handleItemClick}>Plantă carnivoră</a></li>
+                        <li><a className="dropdown-item" href="#" onClick={handleItemClick}>Zonă</a></li>
+                    </ul>
+                </div>
+
+                <div id="input-container">
+                    <div className="form-floating mb-3">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                        <label htmlFor="floatingInput">Filtru</label>
+                    </div>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} beat size="2xl" style={{color: "#307853",}} />
+                </div>
+
+            </div>
+
+            <div id="tableEmployee" style={{top: showSearchForm ? '20%' : '20%'}}>
+                <table className="table table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Denumire</th>
+                        <th scope="col">Tip</th>
+                        <th scope="col">Specie</th>
+                        <th scope="col">Planta Carnivora</th>
+                        <th scope="col">Zona Gradina</th>
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Papadie</td>
+                        <td>Floare</td>
+                        <td>Papadie galbena</td>
+                        <td>Nu</td>
+                        <td>A</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Papadie</td>
+                        <td>Floare</td>
+                        <td>Papadie galbena</td>
+                        <td>Nu</td>
+                        <td>A</td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     )
 }
