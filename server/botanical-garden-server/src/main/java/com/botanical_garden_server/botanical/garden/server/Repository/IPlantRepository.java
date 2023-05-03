2d7490351243880081465808ff4e8db2975188f9
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-05-03 15:29:46 +0200

    IPlantrepository impl

---
+++ server/botanical-garden-server/src/main/java/com/botanical_garden_server/botanical/garden/server/Repository/IPlantRepository.java
@@ -0,0 +1,17 @@
+package com.botanical_garden_server.botanical.garden.server.Repository;
+
+import com.botanical_garden_server.botanical.garden.server.Model.Plant;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IPlantRepository extends CrudRepository<Plant, Long> {
+
+    @Query("SELECT p FROM Plant p WHERE p.{criteria} = :filter")
+    List<Plant> filterPlants(@Param("criteria") String criteria, @Param("filter") String filter);
+}
+
