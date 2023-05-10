494127d9877d7f23474f868e272edd711e3dbb6d
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-05-10 16:51:16 +0200

    pop up message add user

--- client/botanical_garden_client/src/Pages/AddUser.js
+++ client/botanical_garden_client/src/Pages/AddUser.js
@@ -1,7 +1,9 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import httpClient from "./httpClient";
 import {I18nextProvider, useTranslation} from "react-i18next";
 import i18n from "../i18n";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleCheck, faCircleExclamation} from "@fortawesome/free-solid-svg-icons";
 
 export default function AddUser(){
 
@@ -11,23 +13,59 @@ export default function AddUser(){
         password: "",
         role: "",
     });
+    const [popUpMessage, setPopUpMessage] = useState(null);
 
 
     const handleSubmit = async () => {
         try {
-            const response = await httpClient.post(
-                "http://localhost:8080/insertUser",
-                addUserData
-            );
+            const response = await httpClient.post("http://localhost:8080/insertUser", addUserData);
             console.log(response.data);
+
+            if (response.status === 200) {
+                setPopUpMessage({ message: "Success!", isSuccess: true });
+            } else {
+                setPopUpMessage({ message: "An error occurred while submitting the form.", isSuccess: false });
+            }
         } catch (error) {
             console.error(error);
+            setPopUpMessage({ message: "An error occurred while submitting the form.", isSuccess: false });
         }
+    };
+
+    const PopUpMessage = ({ message, isSuccess }) => {
+        const icon = isSuccess ? faCircleCheck : faCircleExclamation;
+        const color = isSuccess ? "#10d14a" : "#ff0000";
+        const [visible, setVisible] = useState(true);
+
+        useEffect(() => {
+            const timeout = setTimeout(() => {
+                setVisible(false);
+            }, 500);
+
+            return () => clearTimeout(timeout);
+        }, []);
+
+        return (
+            <div
+                className={`popup-message ${visible ? 'visible' : ''}`}
+                style={{ color }}
+            >
+                <FontAwesomeIcon icon={icon} size="2xl" />
+                <p>{message}</p>
+            </div>
+        );
     };
 
     return(
         <I18nextProvider i18n={i18n}>
 
+            <div id="message-box">
+
+
+            {popUpMessage && (
+                    <PopUpMessage message={popUpMessage.message} isSuccess={popUpMessage.isSuccess} />
+            )}
+            </div>
             <div className="card" id="addUserCard" style={{ width: "18rem" }}>
                 <ul className="list-group list-group-flush">
 
@@ -86,6 +124,9 @@ export default function AddUser(){
                     >{t("adminPage.addUsBttn")}</button>
 
                 </ul>
+
+
+
             </div>
         </I18nextProvider>
     )
